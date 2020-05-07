a20f11ef29f7a1db3541934099b1c3ebfa94989b
reerant <43791491+reerant@users.noreply.github.com>
2020-05-07 21:46:32 +0300

    add  build

---
+++ build/precache-manifest.e24f866210e99ba1dea78f2775cc357d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad8b1f50a32965429869584ca153a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28e138d7bbfcb73ab688",
+    "url": "/static/css/main.66277810.chunk.css"
+  },
+  {
+    "revision": "edf0de8d59007d5fca29",
+    "url": "/static/js/2.0127288e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0127288e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e138d7bbfcb73ab688",
+    "url": "/static/js/main.eba2dd12.chunk.js"
+  },
+  {
+    "revision": "fe9eb8dbea39d1850969",
+    "url": "/static/js/runtime-main.bdd1f070.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express();
 app.use(cors())
 app.use(express.json());
+app.use(express.static('build'))
 //shows data from request body
 morgan.token("data", function (req, res) {
   return JSON.stringify(req.body);
